7bc3966b164f64af3ba16eec452429387fc76b6f
Blue-Robin-Taken <julianjohnt@gmail.com>
2026-06-06 23:32:09 -0500

    added the basics w react router, tailwind, etc. need to fix build folder and readd the favicon I accidentally removed

--- src/api/search.ts
+++
@@ -1,7 +0,0 @@
-import type { SearchEntry } from '../types/api'
-import { getJSON } from './http'
-
-// GET /-/search-index.json → flat [{title, path}] index built from all articles.
-export function fetchSearchIndex(): Promise<SearchEntry[]> {
-  return getJSON<SearchEntry[]>('/-/search-index.json')
-}
